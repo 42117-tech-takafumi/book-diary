ab7e97d1cc3241a9f49ce20c91f88bbfd6de3102
42117-tech-takafumi <takafumitech@gmail.com>
2025-03-19 20:01:58 +0900

    本を検索する機能_実装
    ・reviewsのコントローラーにsearchアクションの処理を記述
    ・app/models/services/rakuten_books_service.rbを作成し、本を検索するメソッドとジャンルを検索するメソッドを記述
    ・app/views/reviews/index.html.erbの見た目を整えた
    ・app/assets/stylesheets/index.cssを作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,31 @@
 class ReviewsController < ApplicationController
-
+  
   def index
     
   end
 
+  def search
+    #検索条件を変数に格納
+    @search_params = params
+    #binding.pry
+    #入力値に誤りがある場合はエラーメッセージを作成
+    if @search_params[:title_query].blank? && @search_params[:author_query].blank? && @search_params[:isbn_query].blank?
+      @error_message = "Please enter a value for either the Title, Author, or Isbn."
+    elsif @search_params[:isbn_query].present? && !@search_params[:isbn_query].match(/^\d{10}|\d{13}$/)
+      @error_message = "Isbn must be 10 or 13 digits of half-width numbers for the Isbn."
+    else
+      #本を検索する
+      book_search = RakutenBooksService.search_books(@search_params) 
+      @books = book_search[0]
+      @book_counts = {total_hits:book_search[1],page:book_search[2],total_pages:book_search[3]}
+
+      #本を検索出来たらジャンルを検索する
+      if @books.present?
+        @books = RakutenBooksService.search_genres(@books)
+      end
+    end
+
+    render :index
+
+  end
 end
